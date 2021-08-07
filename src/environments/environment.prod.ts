271072c9d3465419405754c9514212171f388624
Ahmad Miqdaad <ahmad.miqdaad@kalsym.com>
2021-08-07 13:15:04 +0800

    remove unused enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,10 @@
 export const environment = {
   production: true,
-  url1:"http://209.58.160.20:3002",
-  url2:"https://api.symplified.biz/v1/user-service",
-  url3:"https://api.symplified.biz/v1/product-service",
-  url4:"https://api.symplified.biz/order-service/v1"
+  name: 'prod',
+  client_id: '',
+  client_secret: '',
+  // url1:"http://209.58.160.20:3002",
+  // url2:"https://api.symplified.biz/v1/user-service",
+  // url3:"https://api.symplified.biz/v1/product-service",
+  // url4:"https://api.symplified.biz/order-service/v1"
 };
